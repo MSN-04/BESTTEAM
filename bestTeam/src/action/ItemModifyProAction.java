83615c52ecbf6073efe1e96e59430aaece9f5d3b
Administrator <Administrator@DESKTOP-GIFA4LK.mshome.net>
2019-05-20 18:31:48 +0900

    2019-05-20 18:32

--- bestTeam/src/action/ItemModifyProAction.java
+++ bestTeam/src/action/ItemModifyProAction.java
@@ -1,16 +1,22 @@
 package action;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
 import svc.ItemModifyProService;
 import vo.ActionForward;
+import vo.UserBean;
 
 public class ItemModifyProAction implements Action {
 /* 아이템 수정 Action (<- FROM itemModifyPro.em )
- 1. Session id 저장
+ 1. Session id 받아 userBean에 저장
+ 2. 수정확인 팝업창 띄우기
  2. ItemModifyProService - isAdmin(id) : isRightAdmin 리턴해줌. 관리자 계정확인
  3. ItemModifyProService - modifyItem(item) : isModifySuccess 리턴해줌. 아이템정보 수정 성공 확인
  4. 아이템정보 수정 성공 시 포워딩 경로 저장 : /shop/product-single.jsp 
@@ -19,19 +25,35 @@ public class ItemModifyProAction implements Action {
 	
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("ItemModifyProAction 시작");
 		
 		ActionForward forward = null;
 		
 		// 1.
 		HttpSession session = request.getSession();
-		String id = (String) session.getAttribute("id");
+		String sessionId = (String) session.getAttribute("id");
+		System.out.println("sessionId : "+ sessionId);
+		
+		UserBean userBean = new UserBean();
+		userBean.setUser_id(sessionId);
+
+		// 2.
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		
+		
+		
+		
+		int item_num = Integer.parseInt(request.getParameter("item_num"));
 		
 		// 2.
 		ItemModifyProService itemModifyProService = new ItemModifyProService();
-		boolean isRightAdmin = itemModifyProService.isAdmin(id);
+		boolean isAdmin = itemModifyProService.isAdmin(sessionId);
 		
-		if(isRightAdmin) {
+		if(isAdmin) {
 			// 패스워드 일치 확인
+			
 		}
 		
 		return null;
---
+++ bestTeam/src/svc/ItemDeleteProService.java
@@ -0,0 +1,53 @@
+package svc;
+
+import static db.JdbcUtil.*;
+import java.sql.Connection;
+import dao.ItemDAO;
+import dao.UserDAO;
+import vo.UserBean;
+
+public class ItemDeleteProService {
+	// item 등록자가 관리자인지 일치여부 확인을 위해 id와 패스워드 확인작업
+	public boolean isAdminWriter(String user_id,String user_pass) throws Exception{
+		System.out.println(" ------ItemDeleteProService--isAdminWriter--");
+		boolean isAdminWriter=false;
+		
+		Connection con=getConnection();
+		ItemDAO itemdao=ItemDAO.getInstance();
+		UserDAO userdao=UserDAO.getInstance();
+		UserBean userBean= new UserBean();
+		
+		
+		itemdao.setConnection(con);
+		isAdminWriter=userdao.isRightUser(userBean);
+		close(con);
+		
+		
+		return isAdminWriter;
+		
+	}
+	
+	//------ item 번호에 해당하는 글 삭제 수행 ----------
+	public boolean deleteItem(int item_num) {
+		System.out.println("--------deleteItem 메서드 -------");
+		boolean isDeleteSuccess=false;
+		Connection con=getConnection();
+		ItemDAO itemdao=ItemDAO.getInstance();
+		itemdao.setConnection(con);
+		
+		int deleteSuccess=itemdao.deleteItem(item_num);
+		System.out.println("deleteSuccess :"+deleteSuccess);
+		
+		// 삭제 성공하면>0 (commit) , 실패하면 rollback하기
+		if(deleteSuccess>0) {
+			commit(con);
+			isDeleteSuccess=true;
+		}else {
+			rollback(con);
+		}
+		
+		return isDeleteSuccess;
+		
+	}
+	
+}
