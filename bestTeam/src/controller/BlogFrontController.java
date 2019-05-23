efe3da2ee15b294478bc7ffc1a4fb5562606efcd
www <www@MELLY>
2019-05-23 18:18:44 +0900

    jiyideum

--- bestTeam/src/controller/BlogFrontController.java
+++ bestTeam/src/controller/BlogFrontController.java
@@ -13,7 +13,7 @@ import action.Action;
 import action.BlogDeleteProAction;
 import action.BlogDetailAction;
 import action.BlogListAction;
-import action.BlogModifyFormAction;
+import action.BlogModifyProAction;
 import action.BlogWriteProAction;
 import vo.ActionForward;
 
