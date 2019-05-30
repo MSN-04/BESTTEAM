274452aaefcb7279b965488a0d01330f1fb4ec05
cyeongb <46704590+cyeongb@users.noreply.github.com>
2019-05-30 20:37:08 +0900

    Update ConfirmCheckoutProAction.java
    yb

--- bestTeam/src/action/ConfirmCheckoutProAction.java
+++ bestTeam/src/action/ConfirmCheckoutProAction.java
@@ -2,14 +2,34 @@ package action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import vo.ActionForward;
+import vo.BuyBean;
+import vo.BuyItemBean;
+import vo.UserBean;
 
 public class ConfirmCheckoutProAction implements Action {
+	
+	//------ CheckoutList에서  해당 게시물을 클릭하면  ---> ConfirmCheckoutProAction으로 온다.
+	
+	
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("ConfirmCheckoutProAction 시작");
+		ActionForward forward=null;
+		BuyItemBean buyItemBean=null;
+		BuyBean buyBean=null;
+		UserBean userBean=null;
+		
+		int buy_num=buyBean.getBuy_num();
+		
+		
+		
+		
+		
+		
 		return null;
 	}
 
