9273c3945e366c905e723137bf48785e428b3dc2
beausong <49707340+beausong@users.noreply.github.com>
2019-05-31 20:02:52 +0900

    misong 190531

--- bestTeam/src/dao/BuyDAO.java
+++ bestTeam/src/dao/BuyDAO.java
@@ -189,6 +189,47 @@ public class BuyDAO {
 	
 	/*-------------------------------------- 미송 ---------------------------------------*/
 	
+		//-- 장바구니의 아이템들 리턴
+	public ArrayList<BuyItemBean> getCartItems(String id) {
+		
+		System.out.println("BuyDAO - getCartItems() 시작");
+		ArrayList<BuyItemBean> cartItems = new ArrayList<>();
+		BuyItemBean buyItemBean = new BuyItemBean();
+		
+		String sql = "SELECT * FROM cart c JOIN item i ON (c.cart_item_num = i.item_num) WHERE c.cart_user_id = ?";
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				// 1개 아이템 정보 가져와 저장
+				buyItemBean = new BuyItemBean();
+				
+				buyItemBean.setItem_buy_num(rs.getInt("item_buy_num"));
+				buyItemBean.setItem_count(rs.getInt("item_count"));
+				buyItemBean.setItem_img(rs.getString("item_img"));
+				buyItemBean.setItem_name(rs.getString("item_name"));
+				buyItemBean.setItem_num(rs.getInt("item_num"));
+				buyItemBean.setItem_price(rs.getInt("item_price"));
+				
+				
+				cartItems.add(buyItemBean);
+			}
+			
+			System.out.println("BuyDAO - getCartItems() 성공");
+			
+		} catch (SQLException e) {
+			System.out.println("BuyDAO - getCartItems() 실패");
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		return cartItems;
+	}
+	
 	/*-------------------------------------- 기홍 ---------------------------------------*/
 	
 	/*-------------------------------------- 영비 ---------------------------------------*/
