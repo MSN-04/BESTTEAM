9ce53528f01c5e00ff03e022fc5347a5630f8878
kwon <kwon@DESKTOP-GIFA4LK>
2019-06-06 18:09:16 +0900

    kwon

---
+++ bestTeam/src/controller/ETCFrontController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.ActionForward;
+
+
+@WebServlet("*.et")
+public class ETCFrontController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+//		System.out.println(requestURI + " " + contextPath +  " " + command);
+		
+		ActionForward forward = null;
+		
+		if(command.equals("/fest.et")) {
+			forward = new ActionForward();
+			forward.setPath("./fest/fest.jsp");
+		} 
+		
+		if(forward != null) {
+			if(forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			}else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				
+				dispatcher.forward(request, response);
+			}
+			
+		}
+	}
+	
+	
+	
+	
+
+}
